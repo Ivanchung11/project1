1432703d7fc5e8dd81aa2b979aa16aeef24995da
Ivanchung11 <chungchivan@gmail.com>
2024-08-27 15:28:07 +0800

    add button in map

--- route-map.js
+++ route-map.js
@@ -1,5 +1,37 @@
 let map;
 
+const chicago = { lat: 22.324480290225303, lng: 114.1626473445726 };
+
+/**
+ * Creates a control that recenters the map on Chicago.
+ */
+function createCenterControl(map) {
+  const controlButton = document.createElement("button");
+
+  // Set CSS for the control.
+  controlButton.style.backgroundColor = "#fff";
+  controlButton.style.border = "2px solid #fff";
+  controlButton.style.borderRadius = "3px";
+  controlButton.style.boxShadow = "0 2px 6px rgba(0,0,0,.3)";
+  controlButton.style.color = "rgb(25,25,25)";
+  controlButton.style.cursor = "pointer";
+  controlButton.style.fontFamily = "Roboto,Arial,sans-serif";
+  controlButton.style.fontSize = "16px";
+  controlButton.style.lineHeight = "38px";
+  controlButton.style.margin = "8px 0 22px";
+  controlButton.style.padding = "0 5px";
+  controlButton.style.textAlign = "center";
+  controlButton.textContent = "Center Map";
+  controlButton.title = "Click to recenter the map";
+  controlButton.type = "button";
+  // Setup the click event listeners: simply set the map to Chicago.
+  controlButton.addEventListener("click", () => {
+    map.setCenter(chicago);
+  });
+  return controlButton;
+}
+
+
 async function initMap() {
   const { Map } = await google.maps.importLibrary("maps");
 
@@ -8,11 +40,19 @@ async function initMap() {
     center: { lat: 22.324480290225303, lng: 114.1626473445726 },
     zoom: 12,
   });
-  const cycTrackLayer = new google.maps.KmlLayer({
-    url: "https://www.td.gov.hk/datagovhk_tis/cycling-information/CYCTRACK.kmz",
-    map: map,
-  });
+  // const cycTrackLayer = new google.maps.KmlLayer({
+  //   url: "https://www.td.gov.hk/datagovhk_tis/cycling-information/CYCTRACK.kmz",
+  //   map: map,
+  // });
+  const centerControlDiv = document.createElement("div");
+  // Create the control.
+  const centerControl = createCenterControl(map);
+
+  // Append the control to the DIV.
+  centerControlDiv.appendChild(centerControl);
+  map.controls[google.maps.ControlPosition.TOP_CENTER].push(centerControlDiv);
 }
 
 initMap();
+// window.initMap = initMap;
 
