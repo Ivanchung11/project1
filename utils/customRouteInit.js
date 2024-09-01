8dfed9bb3edd0579a65986d788407081b25c1848
WilliTam <twy.willi@gmail.com>
2024-09-01 21:42:20 +0800

    initial data run smoothly

--- utils/customRouteInit.js
+++ utils/customRouteInit.js
@@ -24,7 +24,7 @@ const pgClient = new Client({
 
 pgClient.connect();
 
-let createdDate = new Date();
+// let createdDate = new Date();
 // createdDate = moment(createdDate.getTime()).format("YYYY-MM-DD");
 
 var gpx = new gpxParser(); //Create gpxParser Object
@@ -86,15 +86,37 @@ let route5 = {
   isPublic: false,
 };
 
+function GFG_Fun(theDate) {
+  let date = new Date(theDate);
+  return (
+    "TIMESTAMP '" +
+    date.toISOString().split("T")[0]
+     + " " + date.toTimeString().split(" ")[0] + "'"
+  );
+}
+
 async function insertroute(routeObj) {
-  let data = await fs.readFileSync(routeObj.filepath); //import the gpx file as string
+  let data = await fs.promises.readFile(routeObj.filepath, "utf8"); //import the gpx file as string
   gpx.parse(data);
   console.log(gpx.tracks[gpx.tracks.length - 1].name);
+  console.log(gpx.metadata.time);
   console.log("=============");
 
   var distanceArr = gpx.tracks[gpx.tracks.length - 1].distance;
   var totalDistance = distanceArr.total;
   var geopoints = gpx.tracks[gpx.tracks.length - 1].points;
+
+  let recordTime = "";
+  if (gpx.metadata.time) {
+    recordTime = gpx.metadata.time;
+  } else if (geopoints[0].time) {
+    recordTime = geopoints[0].time;
+  }
+
+  if (recordTime != "") {
+    recordTime = GFG_Fun(recordTime);
+    console.log(typeof(recordTime)+": "+recordTime);
+  }
 
   let duration = 0;
 
@@ -118,54 +140,61 @@ async function insertroute(routeObj) {
         route (users_id, route_name, description, star_district_id, end_district_id, road_bicyle_track,
         distance, duration, view_count, public_private, created_at)
     VALUES
-        ((SELECT id from users where name ='${routeObj.uploader}'), '${routeObj.routeName}', '${routeObj.description}', (SELECT id from district where name ='${routeObj.startDistrict}'),(SELECT id from district where name ='${routeObj.endDistrict}'), ${routeObj.isRoad}, ${totalDistance}, ${duration}, 0, ${routeObj.isPublic}, now())`;
+        ((SELECT id from users where name ='${routeObj.uploader}'), '${routeObj.routeName}', '${routeObj.description}', (SELECT id from district where name ='${routeObj.startDistrict}'),(SELECT id from district where name ='${routeObj.endDistrict}'), ${routeObj.isRoad}, ${totalDistance}, ${duration}, 0, ${routeObj.isPublic}, ${recordTime})`;
     await pgClient.query(sql_1);
 
-  let maxlon = geopoints[0].lon;
-  let minlon = geopoints[0].lon;
-  let maxlat = geopoints[0].lat;
-  let minlat = geopoints[0].lat;
+    let maxlon = geopoints[0].lon;
+    let minlon = geopoints[0].lon;
+    let maxlat = geopoints[0].lat;
+    let minlat = geopoints[0].lat;
 
-  for (let i = 0; i < geopoints.length; i++) {
-    let location = `POINT (${geopoints[i].lon} ${geopoints[i].lat})`;
-    let ele = geopoints[i].ele;
-    // let coordTimes = geopoints[i].time;
-    let cumul = distanceArr.cumul[i];
-    const sql = `Insert into path_info (route_id, location, ele, cumul) 
+    for (let i = 0; i < geopoints.length; i++) {
+      let location = `POINT (${geopoints[i].lon} ${geopoints[i].lat})`;
+      let ele = geopoints[i].ele;
+      // let coordTimes = geopoints[i].time;
+      let cumul = distanceArr.cumul[i];
+      const sql = `Insert into path_info (route_id, location, ele, cumul) 
     values ((SELECT id from route where route_name ='${routeObj.routeName}'), $1, $2, $3)`;
-    await pgClient.query(sql, [location, ele, cumul]);
+      await pgClient.query(sql, [location, ele, cumul]);
 
-    if (geopoints[i].lon > maxlon) {
-      maxlon = geopoints[i].lon;
+      if (geopoints[i].lon > maxlon) {
+        maxlon = geopoints[i].lon;
+      }
+      if (geopoints[i].lon < minlon) {
+        minlon = geopoints[i].lon;
+      }
+      if (geopoints[i].lat > maxlat) {
+        maxlat = geopoints[i].lat;
+      }
+      if (geopoints[i].lat < minlat) {
+        minlat = geopoints[i].lat;
+      }
     }
-    if (geopoints[i].lon < minlon) {
-      minlon = geopoints[i].lon;
-    }
-    if (geopoints[i].lat > maxlat) {
-      maxlat = geopoints[i].lat;
-    }
-    if (geopoints[i].lat < minlat) {
-      minlat = geopoints[i].lat;
-    }
-  }
-  let midlon = (minlon + maxlon) / 2;
-  let midlat = (minlat + maxlat) / 2;
-  console.log(midlon, midlat);
+    let midlon = (minlon + maxlon) / 2;
+    let midlat = (minlat + maxlat) / 2;
+    console.log(midlon, midlat);
 
-  let trackCentre = `POINT (${midlon} ${midlat})`;
-  const sql_ctr = `update route set centre = $1
+    let trackCentre = `POINT (${midlon} ${midlat})`;
+    const sql_ctr = `update route set centre = $1
       where route_name =$2 and users_id = (SELECT id from users where name = $3)`;
-  await pgClient.query(sql_ctr, [
-    trackCentre,
-    routeObj.routeName,
-    routeObj.uploader,
-  ]);
-}
+    await pgClient.query(sql_ctr, [
+      trackCentre,
+      routeObj.routeName,
+      routeObj.uploader,
+    ]);
+  }
 }
 
 // insertroute(route1);
-insertroute(route2);
+// insertroute(route2);
 // insertroute(route3);
 // insertroute(route4);
 // insertroute(route5);
 // main();
+
+async function main() {
+  await insertroute(route2);
+  pgClient.end();
+}
+
+main();
--- utils/loadJSONtoDB.js
+++ utils/loadJSONtoDB.js
@@ -1,12 +1,13 @@
 const fs = require("fs");
+// const jsonfile = require("jsonfile");
 const path = require("path");
 const { Client } = require("pg");
 const dotenv = require("dotenv");
 
 dotenv.config();
-console.log(process.env.DB_NAME);
+// console.log(process.env.DB_NAME);
 
-// PostgreSQL connection details 
+// PostgreSQL connection details
 const client = new Client({
   host: "localhost",
   user: process.env.DB_USER,
@@ -17,34 +18,37 @@ const client = new Client({
 
 client.connect();
 
+async function parseJsonPoint(jsonpath, dbTable) {
+  let theFilePath = path.join(__dirname, jsonpath);
 
-//input waterDispenser JSON and parse it by JSON.parse
-let waterDispenserJsonPath = path.join(__dirname, "../data/lcsd_wd_en.json");
+  let theFile = await fs.promises.readFile(theFilePath, "utf8");
 
-let theFile = fs.readFileSync(waterDispenserJsonPath);
-theFile = JSON.parse(theFile);
-console.log(theFile);
+  theFile = await JSON.parse(theFile);
 
-
-//convert to WKT
-const coordinates = theFile.map((point) => {
-  return `POINT(${point.Longitude} ${point.Latitude})`;
-});
-console.log(coordinates);
-
-//save data to sql database
-if (coordinates.length > 0) {
-  coordinates.forEach((coord, index) => {
-    const query = `INSERT INTO water_dispenser (point_coordinates) VALUES (ST_GeogFromText($1))`;
-    client.query(query, [coord], (err, res) => {
-      if (err) {
-        console.error("Error inserting data:", err.stack);
-      } else {
-        console.log(`Data inserted successfully for route ${index + 1}`);
-      }
-    });
+  //convert to WKT
+  const coordinates = theFile.map((point) => {
+    return `POINT(${point.Longitude.trim()} ${point.Latitude.trim()})`;
   });
-} else {
-  console.error("No Point found in the CSV file.");
+
+  //save data to sql database
+  if (coordinates.length > 0) {
+    for (let i = 0; i < coordinates.length; i++) {
+      const query = `INSERT INTO ${dbTable} (point_coordinates) VALUES (ST_GeogFromText($1))`;
+      await client.query(query, [coordinates[i]]);
+      console.log(i + 1, "th uploaded", coordinates[i]);
+    }
+  } else {
+    console.error("No Point found in the CSV file.");
+    client.end();
+  }
+}
+//input waterDispenser JSON and parse it by JSON.parse
+
+// client.end()
+async function main() {
+  await parseJsonPoint("../data/lcsd_wd_en.json", "water_dispenser");
   client.end();
 }
+
+main();
+// "../data/lcsd_wd_en.json"
