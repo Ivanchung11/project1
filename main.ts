da83194d90e0b70adda1fc1655c43984d78c1782
WilliTam <twy.willi@gmail.com>
2024-09-06 17:37:07 +0800

    add null search result alert

--- main.ts
+++ main.ts
@@ -352,27 +352,6 @@ app.post("/search", async function (req: Request, res: Response) {
   try {
     let { startDistricts, endDistricts, isRoads } = req.body;
 
-    // let AllDistricts = [
-    //   "Central and Western",
-    //   "Wan Chai",
-    //   "Eastern",
-    //   "Southern",
-    //   "Yau Tsim Mong",
-    //   "Sham Shui Po",
-    //   "Kowloon City",
-    //   "Wong Tai Sin",
-    //   "Kwun Tong",
-    //   "Kwai Tsing",
-    //   "Tsuen Wan",
-    //   "Tuen Mun",
-    //   "Yuen Long",
-    //   "North",
-    //   "Tai Po",
-    //   "Sha Tin",
-    //   "Sai Kung",
-    //   "Islands",
-    // ];
-
     if (startDistricts == null) {
       startDistricts = ["null"];
     }
